a6f3d882a87d3e995a6bb9f8076f66021c2fa1a4
Shamanguss <46979096+Shamanguss@users.noreply.github.com>
2019-01-23 20:54:00 -0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+var tmi = require('tmi.js');
+var options = 
+{
+	options: 
+	{
+		debug: true
+	},
+	connection: 
+	{
+		cluster: "aws",
+		reconnect: true
+	},
+	identity: 
+	{
+		username: "shamanngus",
+		password: "oauth:rlgglcpdim15hcokvis9rhyds7wvtc",  
+	},
+	channels: ['#johnpittertv', '#gaules',] //ADICIONA OS CANAIS AQ COM '#NOME'
+};
+
+var client = new tmi.client(options);
+
+	client.connect();
+	client.on("connected", function (address, port)
+	{
+		console.log("address: " + address + " port: " + port);
+		client.action("999mateus", "Hello 999mateus!")
+	});
